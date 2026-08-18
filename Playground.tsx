01d95ae25236e9cc78526094af6e0e459299751b
opnsoftware8-maker <opnsoftware8@gmail.com>
2026-08-18 13:43:35 +0700

    Add files via upload

---
+++ Playground.tsx
@@ -0,0 +1,11 @@
+export default function Playground() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">
+        Hello Playground 🚀
+      </h1>
+
+      <p>เอาไว้ทดลองเขียน UI</p>
+    </div>
+  );
+}
